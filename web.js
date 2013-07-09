ca205870d2388c70b1ab1ea72fd66b5835d60eb7
Yuval Cnaan <ycnaan@gmail.com>
2013-07-09 17:15:50 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  fs = require('fs');
+  var msg = fs.readFileSync('index.html').toString();
+  response.send(msg);
 });
 
 var port = process.env.PORT || 5000;
